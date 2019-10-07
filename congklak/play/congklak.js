1c68b1b2ece403cb614ffdff3f54c92f2ca2a1c3
Bunga Amalia <bungasokia@gmail.com>
2019-10-07 22:12:45 +0700

    Render init states in houses

--- congklak/play/congklak.js
+++ congklak/play/congklak.js
@@ -1,5 +1,14 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const congklak = require('congklak')
+const congklak = require('congklak');
+
+congklak.init();
+states = congklak.getState();
+
+for (let i = 0; i < 16; i += 1) {
+  document.getElementById(`ring-${i}`).innerHTML = states[i];
+}
+
+console.log(states);
 
 },{"congklak":2}],2:[function(require,module,exports){
 'use strict';
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const congklak = require('congklak')
+const congklak = require('congklak');
+
+congklak.init();
+states = congklak.getState();
+
+for (let i = 0; i < 16; i += 1) {
+  document.getElementById(`ring-${i}`).innerHTML = states[i];
+}
+
+console.log(states);
