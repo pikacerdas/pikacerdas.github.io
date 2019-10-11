a6c031e409f959d902b536c6faee509982aa95b5
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-10-11 15:02:26 +0700

    Implement click function

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const congklak = require('congklak');
+const congklak = require('congklak')
 
-congklak.init();
-states = congklak.getState();
+congklak.init()
+const states = congklak.getState()
 
 for (let i = 0; i < 16; i += 1) {
-  document.getElementById(`ring-${i}`).innerHTML = states[i];
+  const ring = document.querySelector(`#ring-${i}`)
+  ring.innerHTML = states[i]
+  ring.parentElement.addEventListener('click', () => {
+    const skipRing = i < 7 ? 15 : 7
+    let seedInHand = parseInt(ring.innerHTML)
+    ring.innerHTML = states[i] = 0
+    let currentRing = i + 1
+    while (seedInHand--) {
+      if (currentRing === skipRing) continue
+      const current = document.querySelector(`#ring-${currentRing}`)
+      const seedInRing = parseInt(current.innerHTML)
+      if (seedInHand) current.innerHTML = states[currentRing] = seedInRing + 1
+      else if (seedInRing) {
+        current.innerHTML = states[currentRing] = 0
+        seedInHand = seedInRing + 1
+      }
+      if (states[currentRing + 1] === undefined) currentRing = 0
+      else currentRing++
+    }
+  })
 }
-
-console.log(states);
